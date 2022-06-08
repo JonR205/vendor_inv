bb91d3f98ce54bed3dd3f17325e3ca6ca36b560a
JonR205 <gohan205@gmail.com>
2022-06-08 14:59:12 -0500

    Added funcions for new item page

--- app.py
+++ app.py
@@ -10,21 +10,21 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///vendor.db"
 # app.run(debug=True)
 db = SQLAlchemy(app)
-headings = (
-    "sku",
-    "Product Name",
-    "Product Description",
-    "Cost to Make",
-    "Price",
-    "Category",
-    "Notes",
-)
+# headings = (
+#     "sku",
+#     "Product Name",
+#     "Product Description",
+#     "Cost to Make",
+#     "Price",
+#     "Category",
+#     "Notes",
+# )
 
-data = (
-    ("Jon", "rhine", "3", "4", "5", "6", "7"),
-    ("Jon", "rhine", "3", "4", "5", "6", "7"),
-    ("Jon", "rhine", "3", "4", "5", "6", "7"),
-)
+# data = (
+#     ("Jon", "rhine", "3", "4", "5", "6", "7"),
+#     ("Jon", "rhine", "3", "4", "5", "6", "7"),
+#     ("Jon", "rhine", "3", "4", "5", "6", "7"),
+# )
 
 # DB table for products
 class Products(db.Model):
@@ -37,6 +37,16 @@ class Products(db.Model):
     category = db.Column(db.String(500), unique=False, nullable=False)
     prood_notes = db.Column(db.String(1000), unique=False, nullable=False)
     qty = db.Column(db.Integer)
+
+    def __init__(self, sku, prod_name, prood_description, cost_to_make, price, category, prood_notes, qty) -> None:
+        self.sku = sku
+        self.prod_name = prod_name
+        self.prood_description = prood_description
+        self.cost_to_make = cost_to_make
+        self.price = price
+        self.category = category
+        self.prood_notes = prood_notes
+        self.qty = qty
     
 # p1=Products(sku=1234,prod_name='shirt', prood_description='holloween specials',cost_to_make=12.55,price=19.99,category='clothing',prood_notes='All sales are final!',qty=2)
 # p2=Products(sku=4523,prod_name='Jason Jacket', prood_description='Friday the 13th decals',cost_to_make=22.75,price=39.99,category='clothing',prood_notes='All sales are final!',qty=2)
@@ -82,10 +92,12 @@ def prod_details(sku_num):
     return render_template("prod_details.html", product=product)
 
 
-# @app.route("/sku_search_page", methods=["POST", "GET"])
-# def sku_search():
-#     if request.method == "POST":
-#         sku_num = request.form["sku"]
-#         return redirect(url_for("prod_details", sku_num=sku_num))
-#     else:
-#         return render_template("sku_search_page.html")
+@app.route("/new_product_page", methods=["POST", "GET"])
+def sku_search():
+    if request.method == "POST":
+        np = Products(sku=request.form["sku"],prod_name=request.form["Product Name"], prood_description=request.form["Product Description"],cost_to_make=request.form["Cost to Make"],price=request.form["Selling Price"],category=request.form["Product Category"],prood_notes=request.form["Quantity"],qty=request.form["Notes"])
+        db.session.add(np)
+        db.session.commit()
+        return redirect(url_for("prod_list"))
+    else:
+        return render_template("new_product_page.html")
