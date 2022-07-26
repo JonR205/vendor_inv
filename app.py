4ad23290c344010377cfe72ba5b07ba2dc5ab41f
JonR205 <gohan205@gmail.com>
2022-07-26 15:01:02 -0500

    Push

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-from cmath import e
-from distutils.log import debug
-from email.policy import default
+from datetime import datetime
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from sqlalchemy import DateTime
-import requests
-from datetime import datetime, date
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///vendor.db"
-# app.run(debug=True)
+app.run(debug=True)
+app.templates_auto_reload = True
 db = SQLAlchemy(app)
+
 
 # DB table for products
 class Products(db.Model):
@@ -26,16 +23,14 @@ class Products(db.Model):
     event_inventory = db.relationship("EventInventory", backref="products")
 
     def __init__(
-        self,
-        # sku,
-        prod_name,
-        prood_description,
-        cost_to_make,
-        price,
-        category,
-        prood_notes,
-        qty,
-        # event_inventory,
+            self,
+            prod_name,
+            prood_description,
+            cost_to_make,
+            price,
+            category,
+            prood_notes,
+            qty,
     ) -> None:
         # self.sku = sku
         self.prod_name = prod_name
@@ -65,14 +60,14 @@ class Events(db.Model):
     event_event_inventory = db.relationship("EventInventory", backref="events")
 
     def __init__(
-        self,
-        event_name,
-        event_description,
-        event_start_date,
-        event_end_date,
-        # products_brought,
-        # products_sold,
-        event_notes,
+            self,
+            event_name,
+            event_description,
+            event_start_date,
+            event_end_date,
+            # products_brought,
+            # products_sold,
+            event_notes,
     ) -> None:
         # self.sku = sku
         self.event_name = event_name
@@ -84,7 +79,7 @@ class Events(db.Model):
         self.event_notes = event_notes
 
     def __repr__(self):
-        return f"Event Name: {self.event_name} Description: {self.event_description} Start Datew: {self.event_start_date} End Date: {self.event_end_date} Notes: {self.event_notes}  event_inventory: {self.event_event_inventory}"
+        return f"Event Name: {self.event_name} Description: {self.event_description} Start Date: {self.event_start_date} End Date: {self.event_end_date} Notes: {self.event_notes}  event_inventory: {self.event_event_inventory}"
 
 
 # DB table for Inventory Brought to Events
@@ -105,6 +100,9 @@ class EventInventory(db.Model):
         return f"ID: {self.event_inv_id} SKU: {self.sku_num} Qty Brought: {self.qty_brought} Qty sold: {self.qty_sold} Event Name: {self.event_name}"
 
 
+db.create_all()
+
+
 # Routes for webpages
 
 # Home page lists all products in table
@@ -113,111 +111,86 @@ def prod_list():
     products = Products.query.order_by(Products.sku)
     if request.method == "POST":
         sku_num = request.form["sku"]
-        return redirect(url_for("prod_details", sku_num=sku_num))
+        product = Products.query.get_or_404(sku_num)
+        db.session.delete(product)
+        db.session.commit()
+        return render_template("prod_list_page.html", products=products)
     else:
         return render_template("prod_list_page.html", products=products)
 
 
-# Event lists all events in table
-@app.route("/events_list_page", methods=["POST", "GET"])
-def event_list():
-    events = Events.query.order_by(Events.id)
-    return render_template("event_list_page.html", events=events)
-    # test
+# Product detail page shows single SKU and all details
+@app.route("/prod_details<sku_num>", methods=["GET", "POST"])
+def prod_details(sku_num):
+    product = Products.query.filter_by(sku=str(sku_num)).first()
+    return render_template("prod_details.html", product=product)
 
 
-# Event Inventory lists everythiong in table
-@app.route("/event_inventory_list_page", methods=["POST", "GET"])
-def event_inventory_list():
-    products = Products.query.order_by(Products.sku)
-    events = Events.query.order_by(Events.id)
-    events_inventory = EventInventory.query.order_by(EventInventory.event_inv_id)
-    return render_template(
-        "event_inventory_list_page.html",
-        events=events,
-        products=products,
-        events_inventory=events_inventory,
-    )
+# Page is a view of products filter by a give type and parameter
+@app.route("/prod_filter_by", methods=["GET", "POST"])
+def prod_filter_by():
+    filter_type = request.args.get('filter_type')
+    pram = request.args.get('pram')
+
+    if filter_type == "price":
+        products = Products.query.filter_by(price=str(pram)).all()
+    if filter_type == "cost":
+        products = Products.query.filter_by(cost_to_make=str(pram)).all()
+    if filter_type == "category":
+        products = Products.query.filter_by(category=str(pram)).all()
+
+    return render_template("prod_list_page.html", products=products)
 
 
 # Page to add new product to Products table
 @app.route("/new_product_page", methods=["POST", "GET"])
-def sku_search():
+def new_product():
     if request.method == "POST":
         np = Products(
-            # sku=request.form["sku"],
-            prod_name=request.form["Product Name"],
-            prood_description=request.form["Product Description"],
-            cost_to_make=request.form["Cost to Make"],
-            price=request.form["Selling Price"],
-            category=request.form["Product Category"],
-            prood_notes=request.form["Notes"],
-            qty=request.form["Quantity"],
+            prod_name=request.form["productName"],
+            prood_description=request.form["productDescription"],
+            cost_to_make=request.form["productCost"],
+            price=request.form["productPrice"],
+            category=request.form["productCategory"],
+            prood_notes=request.form["productNotes"],
+            qty=request.form["productQuantity"],
         )
         db.session.add(np)
         db.session.commit()
         return redirect(url_for("prod_list"))
     else:
-        return render_template("new_product_page.html")
+        return render_template("product_form_page.html", pu_curr=None)
 
 
-@app.route("/new_event_inventory_page", methods=["POST", "GET"])
-def event_inv_crteate():
+# page to update product details
+@app.route("/prod_update<sku_num>", methods=["GET", "POST"])
+def prod_update(sku_num):
     if request.method == "POST":
-        nei = EventInventory(
-            sku_num=request.form["SKU Number"],
-            qty_brought=request.form["QTY Brought"],
-            qty_sold=request.form["Event QTY Sold"],
-            event_name=request.form["Event Name"],
+        up = Products.query.filter_by(sku=sku_num)
+        up.update(
+            dict(
+                prod_name=request.form["Product Name"],
+                prood_description=request.form["Product Description"],
+                cost_to_make=request.form["Cost to Make"],
+                price=request.form["Selling Price"],
+                category=request.form["Product Category"],
+                prood_notes=request.form["Notes"],
+                qty=request.form["Quantity"],
+            )
         )
-        pu = Products.query.filter_by(sku=str(request.form["SKU Number"]))
-        # puq = pu.qty
-        pu.update(dict(qty=request.form["QTY Brought"]))
-        db.session.add(nei)
         db.session.commit()
-        return redirect(url_for("event_inventory_list"))
+        return redirect(url_for("prod_list"))
     else:
-        return render_template("new_event_inventory_page.html")
+        pu_curr = Products.query.filter_by(sku=sku_num).first()
+        return render_template("product_form_page.html", pu_curr=pu_curr)
 
 
-@app.route("/new_event_page", methods=["POST", "GET"])
-def new_event():
-    if request.method == "POST":
-        ne = Events(
-            event_name=request.form["Event Name"],
-            event_description=request.form["Event Description"],
-            event_start_date=datetime.strptime(
-                request.form["Event Start Date"], "%Y-%m-%d"
-            ),
-            event_end_date=datetime.strptime(
-                request.form["Event End Date"], "%Y-%m-%d"
-            ),
-            # products_brought=request.form["Products Brought"],
-            # products_sold=request.form["Products Sold"],
-            event_notes=request.form["Event Notes"],
-        )
-        db.session.add(ne)
-        db.session.commit()
-        return redirect(url_for("event_list"))
-    else:
-        return render_template("new_event_page.html")
-
-
-# Page is a view of like priced items accessed by clicking on price from any product list
-@app.route("/prod_filter_by_price_<price_num>", methods=["GET", "POST"])
-def prod_filter_by_price(price_num):
-    products = Products.query.filter_by(price=str(price_num)).all()
-    return render_template("prod_filter_by_price.html", products=products)
-
-
-# Page is a view of items with same category accessed by clicking on price from any product list
-@app.route("/prod_filter_by_category_<category>", methods=["GET", "POST"])
-def prod_filter_by_category(category):
-    products = Products.query.filter_by(category=str(category)).all()
-    return render_template("prod_filter_by_category.html", products=products)
-
-
-# Page to add new product to Products table
+# Event lists all events in table
+@app.route("/event_list_page", methods=["POST", "GET"])
+def event_list():
+    events = Events.query.order_by(Events.id)
+    return render_template("event_list_page.html", events=events)
+    # test
 
 
 # Event detail page shows single event and all details
@@ -246,13 +219,6 @@ def event_details(id_num):
         return render_template(
             "event_details.html", event=event, events_inventory=events_inventory
         )
-
-
-# Product detail page shows single SKU and all details
-@app.route("/prod_details<sku_num>", methods=["GET", "POST"])
-def prod_details(sku_num):
-    product = Products.query.filter_by(sku=str(sku_num)).first()
-    return render_template("prod_details.html", product=product)
 
 
 @app.route("/modal_<event_inv_id_num>_<sku_num_num>", methods=["GET", "POST"])
@@ -286,24 +252,59 @@ def modal(event_inv_id_num, sku_num_num):
         return render_template("modal.html", ei=ei, ei_current_qty=ei_current_qty)
 
 
-# page to update product details
-@app.route("/prod_update<sku_num>", methods=["GET", "POST"])
-def prod_update(sku_num):
-    pu_curr = Products.query.filter_by(sku=sku_num).first()
-    up = Products.query.filter_by(sku=sku_num)
+@app.route("/new_event_page", methods=["POST", "GET"])
+def new_event():
     if request.method == "POST":
-        up.update(
-            dict(
-                prod_name=request.form["Product Name"],
-                prood_description=request.form["Product Description"],
-                cost_to_make=request.form["Cost to Make"],
-                price=request.form["Selling Price"],
-                category=request.form["Product Category"],
-                prood_notes=request.form["Notes"],
-                qty=request.form["Quantity"],
-            )
+        ne = Events(
+            event_name=request.form["eventName"],
+            event_description=request.form["eventDescription"],
+            event_start_date=datetime.strptime(
+                request.form["eventStartDate"], "%Y-%m-%d"
+            ),
+            event_end_date=datetime.strptime(
+                request.form["eventEndDate"], "%Y-%m-%d"
+            ),
+            event_notes=request.form["eventNotes"],
         )
+        db.session.add(ne)
         db.session.commit()
-        return redirect(url_for("prod_list"))
+        return redirect(url_for("event_list"))
     else:
-        return render_template("prod_update.html", pu_curr=pu_curr)
+        return render_template("new_event_page.html")
+
+
+# -------------------------------------------------------------------------------
+
+# Event Inventory lists everythiong in table
+@app.route("/event_inventory_list_page", methods=["POST", "GET"])
+def event_inventory_list():
+    products = Products.query.order_by(Products.sku)
+    events = Events.query.order_by(Events.id)
+    events_inventory = EventInventory.query.order_by(EventInventory.event_inv_id)
+    return render_template(
+        "event_inventory_list_page.html",
+        events=events,
+        products=products,
+        events_inventory=events_inventory,
+    )
+
+
+@app.route("/new_event_inventory_page", methods=["POST", "GET"])
+def event_inv_crteate():
+    if request.method == "POST":
+        nei = EventInventory(
+            sku_num=request.form["SKU Number"],
+            qty_brought=request.form["QTY Brought"],
+            qty_sold=request.form["Event QTY Sold"],
+            event_name=request.form["Event Name"],
+        )
+        pu = Products.query.filter_by(sku=str(request.form["SKU Number"]))
+        # puq = pu.qty
+        pu.update(dict(qty=request.form["QTY Brought"]))
+        db.session.add(nei)
+        db.session.commit()
+        return redirect(url_for("event_inventory_list"))
+    else:
+        return render_template("new_event_inventory_page.html")
+
+# Page to add new product to Products table
